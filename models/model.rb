01640591afbccb31c30b64692d5fbaf732e20e9c
codered14 <maxwellbdowd@outlook.com>
2015-08-27 18:22:46 +0000

    respond

--- models/model.rb
+++ models/model.rb
@@ -1,32 +1,30 @@
 class Result
   def response(experience, technique, body_part)
-    if experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-    
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    elsif experience == ""&&technique == ""&&body_part == ""
-      
-    else experience == ""&&technique == ""&&body_part == ""
-      
+    if level == "beginner"&&technique == "grappling"&&body_part == "head"
+
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
+
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
+
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
+
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "mid-section"
+
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "mid-section"
+
+    elsif level == "advanced"&&technique == "striking"&&body_part == "mid-section"
+
+    elsif level == "advanced"&&technique == "striking"&&body_part == "mid-section"
+
+    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
+     
+    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
+
+    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
+
+    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
+
     end
-    end 
+    end
 end
 
