d910801f3e055702abee0c30c32e3a15210d2b82
Epocalyse <hcohen123@gmail.com>
2015-08-27 17:49:08 +0000

    second edit

--- application_controller.rb
+++ application_controller.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require_relative 'models/model.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
@@ -6,4 +7,9 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/respond' do
+    puts params
+  end
+  
 end
