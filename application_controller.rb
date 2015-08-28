6b22387ccd8acabb0d640c9b232481533c30f19c
codered14 <maxwellbdowd@outlook.com>
2015-08-28 13:01:50 +0000

    asskicking

--- application_controller.rb
+++ application_controller.rb
@@ -7,9 +7,13 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   post '/respond' do
     puts params
+    image_maker=Result.new
+    @image=image_maker.response(params[:level],params[:technique],params[:body_part])
+    puts @image
+    erb :respondAGH
   end
-  
+
 end
--- models/model.rb
+++ models/model.rb
@@ -1,29 +1,29 @@
 class Result
-  def response(experience, technique, body_part)
-    if level == "beginner"&&technique == "grappling"&&body_part == "head"
-
+  def response(level, technique, body_part)
+    if level == "advanced"&&technique == "grappling"&&body_part == "head"
+      return "AGH.jpg"
+    elsif level == "beginner"&&technique == "striking"&&body_part == "head"
+      return "BSH.jpg"
+    elsif level == "advanced"&&technique == "striking"&&body_part == "head"
+      return "ASH.jpg"
     elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
-
-    elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
-
-    elsif level == "beginner"&&technique == "grappling"&&body_part == "head"
-
+      return "BGH.jpg"
+    elsif level == "advanced"&&technique == "grappling"&&body_part == "mid-section"
+      return "AGM.jpg"
+    elsif level == "beginner"&&technique == "striking"&&body_part == "mid-section"
+      return "BSM.jpg"
     elsif level == "beginner"&&technique == "grappling"&&body_part == "mid-section"
-
-    elsif level == "beginner"&&technique == "grappling"&&body_part == "mid-section"
-
+      return "BGM.jpg"
     elsif level == "advanced"&&technique == "striking"&&body_part == "mid-section"
-
-    elsif level == "advanced"&&technique == "striking"&&body_part == "mid-section"
-
+      return "ASM.jpg"
+    elsif level == "advanced"&&technique == "grappling"&&body_part == "legs"
+      return "AGL.jpg"
+    elsif level == "beginner"&&technique == "striking"&&body_part == "legs"
+      return "BSL.jpg"
     elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
-     
-    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
-
-    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
-
-    elsif level == "advanced"&&technique == "striking"&&body_part == "legs"
-
+      return "ASL.jpg"
+    elsif level == "beginner"&&technique == "grappling"&&body_part == "legs"
+      return "BGL.jpg"
     end
     end
 end
